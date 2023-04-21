74bcce1de770feced6db015d53f65a337b7bf72b
Danik616 <ddanielvargas2011@gmail.com>
2023-04-21 10:46:38 -0500

    Se agregan comentarios de explicacion del codigo

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+/* En el jsconfig.json, indicamos unas almohadillas para no tener que meter las rutas completas en cada
+import, en el documento decimos que la almohadilla #Lib es igual a la ruta. Se importa en el package.json
+*/ 
 import { printHello } from '#Lib/hello.js';
 
 printHello();
+
+// en el package.json se instalan ciertas dependencias como husky, eslint y prettier por lo tanto
+// estos permiten formatear el documento de una forma estandar, esto se configura en la linea 34 de package,json
+// donde le indicamos que a los archivos js los formatee con eslint y a los JS y MD con prettier
+// El proyecto esta configurado en ECSMAscript module
