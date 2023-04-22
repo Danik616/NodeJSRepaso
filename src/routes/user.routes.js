68d48ea9fe149b484591ec32b6c7cdaf0413acba
Danik616 <ddanielvargas2011@gmail.com>
2023-04-22 14:51:26 -0500

    Se hace el controlador de update-email y update-password

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,15 +2,19 @@ import userLoginDTO from '#Dto/user-login.dto.js'
 import userJWTDTO from '#Dto/user-JWT.dto.js'
 import userRegisterDTO from '#Dto/user-register.dto.js'
 import userUpdateDataDTO from '#Dto/user-update-data.dto.js'
+import userUpdateEmailDTO from '#Dto/user-update-email.dto.js'
+import userUpdatePasswordDTO from '#Dto/user-update-password.dto.js'
 import userLoginController from '#Controllers/user-login.controller.js'
 import userRegisterController from '#Controllers/user-register.controller.js'
 import userProfileController from '#Controllers/user-profile.controller.js'
 import userUpdateDataController from '#Controllers/user-update-data.controller.js'
+import userUpdateEmailController from '#Controllers/user-update-email.controller.js'
 
 // import userUnregisterDTO from '#Dto/user-unregister.dto.js'
-// import userUpdateEmailDTO from '#Dto/user-update-email.dto.js'
-// import userUpdatePasswordDTO from '#Dto/user-update-password.dto.js'
+
 import {Router} from 'express'
+import userUpdatePasswordController from '#Controllers/user-update-password.controller.js'
+
 
 
 
@@ -22,8 +26,8 @@ userRouter.post("/login",userLoginDTO, userLoginController)
 
 userRouter.get("/profile", userJWTDTO, userProfileController)
 userRouter.patch("/update-data",userJWTDTO, userUpdateDataDTO, userUpdateDataController)
-// userRouter.patch("/update-email",userJWTDTO, userUpdateEmailDTO, userUpdateEmailController)
-// userRouter.patch("/update-password",userJWTDTO, userUpdatePasswordDTO, userUpdatePasswordController)
+userRouter.patch("/update-email",userJWTDTO, userUpdateEmailDTO, userUpdateEmailController)
+userRouter.patch("/update-password",userJWTDTO, userUpdatePasswordDTO, userUpdatePasswordController)
 // userRouter.delete("/unsubscribe", userUnregisterDTO, userUnregisterController)
 
 export default userRouter
