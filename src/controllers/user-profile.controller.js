73e25ec2127fa242381c4f16a1cab6902f15b632
Danik616 <ddanielvargas2011@gmail.com>
2023-04-22 14:08:24 -0500

    Se corrigen errores de user-profile.controller.js

--- src/controllers/user-profile.controller.js
+++ src/controllers/user-profile.controller.js
@@ -1,12 +1,10 @@
 import UserModel from "#Schemas/user.schema.js";
 
-// Se instala la libreria bcrypt. npm i bcrypt
 const userProfileController = async (req, res) => {
-    // Habia un error y es que al pedir el id que nosotros le habiamos enviado en jwt se estaba llamando a req.body
-    // y simplemente tocaba llamara a req
+    
     const { id } =req;
     const existingUserByID = await UserModel.findById(id).exec()
-    if(!existingUserByID) return res.status(400).send("Usuario no autori")
+    if(!existingUserByID) return res.status(400).send("Usuario no autorizado")
 
     const {_id, name, surname, email} =existingUserByID
 
